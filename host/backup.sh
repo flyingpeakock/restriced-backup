43aac51f4670fed985e58a223433c709a6da2c6f
Philip Johansson <philipphuket@gmail.com>
2022-08-14 11:36:31 +0200

    didnt properly finish case and while loop

--- host/backup.sh
+++ host/backup.sh
@@ -19,6 +19,9 @@ while getopts ':k:r:u:s:n:' OPTION; do
             echo "Usage: $(basename $0) [-k keyfile] [-r remote] [-u user] [-s snapshot directory] [-n number of snapshots to keep]"
             echo "-s and -n only necessary if local machine uses btrfs snapshots for backups"
             exit 0
+            ;;
+    esac
+done
 
 mydir=${0:a:h}
 btrfs subvolume show / > /dev/null 2>&1 && $mydir/backup-btrfs.sh "$keyfile" "$remote" "$user" "$snapshotdir" "$snapshotlim" || $mydir/backup-rsync.sh "$keyfile" "$remote" "$user"
