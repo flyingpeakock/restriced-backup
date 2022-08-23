700da14c61215f64370b8a837d67a9cd749f9352
Philip Johansson <philipphuket@gmail.com>
2022-08-23 20:38:39 +0200

    using glob instaed of ls to list files

--- host/backup-btrfs.sh
+++ host/backup-btrfs.sh
@@ -4,7 +4,7 @@ keyfile="$1"
 remote="$2"
 user="$3"
 snapshots="$4"
-snapshotlim=$(expr "$5" + 1)
+snapshotlim="$5"
 date=$(date --iso-8601)
 
 die() {
@@ -46,48 +46,49 @@ fi
 # Create snapshots of root and home subvolumes
 create_snapshot() {
     # Only keeping one snapshot per day
-    if [ ! -d $3/$1 ]; then
-        btrfs subvolume snapshot -r $2 $3/$1
+    if [ ! -d "$3"/"$1" ]; then
+        btrfs subvolume snapshot -r "$2" "$3"/"$1"
     fi
 }
 
 # Remove all but latest 21 snapshots
 remove_snapshot() {
-    OLDFILES=$(ls -r $1 | tail -n "$snapshotlim")
+    FILES=("$1"/*)
+    OLDFILES=($FILES[0,-21])
     for file in $OLDFILES; do
-        btrfs subvolume delete $1/$file
+        btrfs subvolume delete "$file"
     done
 }
 
 # Send snapshot to remote
 send_snapshot() {
     # parent=$(ssh $user@$remote sudo ls $2/$HOST/$1 | tail -n 1)
-    parent=$(ssh $user@$remote btrfs $HOST parent)
-    if [ -z $parent ] || [ ! -d $snapshots/$1/$parent ]; then
+    parent=$(ssh "$user"@"$remote" btrfs "$HOST" parent)
+    if [ -z "$parent" ] || [ ! -d "$snapshots"/"$1"/"$parent" ]; then
         # Parent does not exist, do not send incremental snapshot
-        btrfs send $snapshots/$1/$date | ssh $user@$remote btrfs $HOST receive $1
-    elif [ ! $parent = $date ]; then
-        btrfs send -p $snapshots/$1/$parent $snapshots/$1/$date | ssh $user@$remote btrfs $HOST receive $1
+        btrfs send "$snapshots"/"$1"/"$date" | ssh "$user"@"$remote" btrfs "$HOST" receive "$1"
+    elif [ ! "$parent" = "$date" ]; then
+        btrfs send -p "$snapshots"/"$1"/"$parent" "$snapshots"/"$1"/"$date" | ssh "$user"@"$remote" btrfs "$HOST" receive "$1"
     else
         printf "Snapshot $1/$date already exists on $remote\n"
     fi
 }
 
-create_snapshot $date / $snapshots/root
-create_snapshot $date /home $snapshots/home
+create_snapshot "$date" / "$snapshots"/root
+create_snapshot "$date" /home "$snapshots"/home
 
-remove_snapshot $snapshots/root
-remove_snapshot $snapshots/home
+remove_snapshot "$snapshots"/root
+remove_snapshot "$snapshots"/home
 
 # Checking if remote is online
-ping -c 1 $remote > /dev/null 2>&1 || exit 0
+ping -c 1 "$remote" > /dev/null 2>&1 || exit 0
 
 # Decrypting the drive before
 # cat /etc/backup.key | ssh $user@$remote sudo decrypt.sh
-cat $keyfile | ssh $user@$remote open $HOST
+cat "$keyfile" | ssh "$user"@"$remote" open "$HOST"
 
 send_snapshot root 
 send_snapshot home 
 
 # Encrypt drive when done
-ssh $user@$remote close $HOST
+ssh "$user"@"$remote" close "$HOST"
